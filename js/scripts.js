6d4ba494f812fe2772ce8d460f8982e4e0ef5098
Charley McGowan and Quinn Miller <charley@>
2018-09-12 18:38:37 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -10,11 +11,34 @@ var multiply = function(number1, number2) {
   return number1*number2;
 };
 
-var devide = function(number1, number2) {
+var divide = function(number1, number2) {
   return number1/number2;
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = devide(number1, number2);
-alert(result);
+
+var result = divide(number1, number2);
+$()
+//Business
+//-------------
+//Front-end
+$(document).ready(function() {
+  var x = 0;
+
+  $("button#1")
+  $("button#2")
+  $("button#3")
+  $("button#4")
+  $("button#5")
+  $("button#6")
+  $("button#7")
+  $("button#8")
+  $("button#9")
+  $("button#0")
+  $("button#add")
+  $("button#subtract")
+  $("button#multiply")
+  $("button#divide")
+  $("button#equals")
+
+});
+//Front-end
