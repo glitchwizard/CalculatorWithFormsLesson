ac80393184883166009a28a6b93cde30d760a6db
Charley McGowan and Quinn Miller <quinn@>
2018-09-12 20:52:54 -0700

    one number one function and equals working

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 //Business
-var add = function(number1, number2) {
-  return number1 + number2;
+var number1 = 0;
+var number2 = 0;
+var output = 0;
+
+var add = function(number1,number2) {
+    output = number1 + number2;
 };
 
 var subtract = function(number1, number2) {
@@ -13,32 +17,48 @@ var multiply = function(number1, number2) {
 
 var divide = function(number1, number2) {
   return number1/number2;
-}
+};
 
-
-var result = divide(number1, number2);
-$()
 //Business
+
 //-------------
+
 //Front-end
-$(document).ready(function() {
-  var x = 0;
 
-  $("button#1")
-  $("button#2")
-  $("button#3")
-  $("button#4")
-  $("button#5")
-  $("button#6")
-  $("button#7")
-  $("button#8")
-  $("button#9")
-  $("button#0")
-  $("button#add")
-  $("button#subtract")
-  $("button#multiply")
-  $("button#divide")
-  $("button#equals")
+var num1 = 0;
+var num2 = 0;
+var op = 0;
 
+$(function () {
+  $('#7').on('click', function () {
+    var text2 = $('#inputfield');
+    if (text2.val() === "0") {
+          text2.val("7");
+    }
+    else {
+      text2.val(text2.val() + "7");
+    }
+  });
+
+  $("#add").on('click', function () {
+    num1 = parseInt($("#inputfield").val());
+    var text3 = $('#inputfield');
+    text3.val("0");
+    op = 1;
+  });
+  $("#equals").on('click', function() {
+    num2  = parseInt($("#inputfield").val());
+    if (op === 1) {
+      add(num1,num2);
+    }
+    else if (op === 2) {
+      subtract(num1,num2);
+    }
+    else if (op === 3) {
+      multiply(num1,num2);
+    }
+    else if (op === 4) {
+      divide (num1,num2);
+    }
+  })
 });
-//Front-end
